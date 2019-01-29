88fa6426bf5e594ccf1d1e4e7803305951f8ab60
Tim <talbert1209@gmail.com>
2019-01-29 16:58:00 -0600

    Remove abstraction layers so I can try again

---
+++ LearnLocalDB.DTO/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnLocalDB.DTO
+{
+    public class Customer
+    {
+        public System.Guid CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        // ReSharper disable once InconsistentNaming
+        public string Postal_Code { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ LearnLocalDB.Domain/CustomersManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearnLocalDB.Persistence;
+
+namespace LearnLocalDB.Domain
+{
+    public class CustomersManager
+    {
+        //public static List<Persistence.Customer> GetCustomers()
+        //{
+        //    var customers = Persistence.CustomersRepository.GetCustomers();
+        //    return customers;
+        //}
+    }
+}
